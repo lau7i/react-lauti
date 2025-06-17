c02fe177685174133a9a3f94863bcf2eda54d7a5
Lautaro Iribarria <lauleoboca@gmail.com>
2025-06-17 18:40:12 -0300

    elimando NavLink que no voy a usar

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, NavLink } from "react-router";
+import { useParams } from "react-router";
 import ItemDetail from "./ItemDetail";
 
 function ItemDetailContainer() {
